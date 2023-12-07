d588a8805fc22649680ceb9a7b8cbb1b298d8995
Connor Thompson <connort2004@gmail.com>
2023-12-07 08:41:42 -0700

    histogram

--- streamlit.py
+++ streamlit.py
@@ -3,25 +3,28 @@ import numpy as np
 import streamlit as st
 import plotly.express as px
 
-st.title('Popular Names')
+st.title('Predicting Football Scores at Halftime')
+st.subheader('Preliminary Data Visualization')
 
-url = 'https://github.com/esnt/Data/raw/main/Names/popular_names.csv'
-df = pd.read_csv(url)
+df = pd.read_csv('cbsFootballData.csv')
 
-selected_name = st.text_input('Enter a name','John') # John is default
-name_df = df[df['name'] == selected_name]
-if name_df.empty:
-    st.write('No data for this name')
-else:
-    fig = px.line(name_df,x='year',y='n',color='sex')
-    st.plotly_chart(fig)
+# selected_name = st.text_input('Enter a name','John') # John is default
+# name_df = df[df['name'] == selected_name]
+# if name_df.empty:
+#     st.write('No data for this name')
+# else:
+#     fig = px.line(name_df,x='year',y='n',color='sex')
+#     st.plotly_chart(fig)
+
+fig = px.histogram(df,'final_differential')
+st.plotly.chart(fig)
     
 
-selected_year = st.select_slider('Select a year',df['year'].unique())
-year_df = df[df['year'] == selected_year]
-girlnames = year_df[year_df['sex'] == 'F'].sort_values(by='n',ascending=False).head(5)['name'].reset_index(drop=True)
-boynames = year_df[year_df['sex'] == 'M'].sort_values(by='n',ascending=False).head(5)['name'].reset_index(drop=True)
-topnames = pd.concat([boynames,girlnames],axis=1)
-topnames.columns=['boy','girl']
-st.write(f'Top names in {selected_year}:')
-st.dataframe(topnames)
+# selected_year = st.select_slider('Select a year',df['year'].unique())
+# year_df = df[df['year'] == selected_year]
+# girlnames = year_df[year_df['sex'] == 'F'].sort_values(by='n',ascending=False).head(5)['name'].reset_index(drop=True)
+# boynames = year_df[year_df['sex'] == 'M'].sort_values(by='n',ascending=False).head(5)['name'].reset_index(drop=True)
+# topnames = pd.concat([boynames,girlnames],axis=1)
+# topnames.columns=['boy','girl']
+# st.write(f'Top names in {selected_year}:')
+# st.dataframe(topnames)
