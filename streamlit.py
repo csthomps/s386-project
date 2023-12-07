ba4d898177fcedafe28a30a75fa2dc98770511c2
Connor Thompson <connort2004@gmail.com>
2023-12-07 09:00:00 -0700

    ?

--- streamlit.py
+++ streamlit.py
@@ -17,7 +17,7 @@ df = pd.read_csv('cbsFootballData.csv')
 #     st.plotly_chart(fig)
 nbins = df['final_differential'].max() - df['final_differential'].min() + 1
 st.text(f'{nbins}')
-fig = px.histogram(df,'final_differential',nbins=30)
+fig = px.histogram(df,'final_differential',nbins=nbins-1)
 st.plotly_chart(fig)
     
 
